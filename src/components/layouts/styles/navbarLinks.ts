91133701cf116159f2a31cf0bc3dc1effa193e9c
Ein <wishawin@gmail.com>
2026-08-24 16:40:54 +0100

    Hide Timeline

--- src/components/layouts/styles/navbarLinks.ts
+++ src/components/layouts/styles/navbarLinks.ts
@@ -5,7 +5,7 @@ export const navbarLinks = [
   { href: '/', label: 'Home' },
   { href: '/education', label: 'Education' },
   { href: '/stack', label: 'Stack' },
-  { href: '/timeline', label: 'Timeline' },
+  // { href: '/timeline', label: 'Timeline' },
   { href: '/projects', label: 'Projects' },
   // Clicking "Experiences" should open the Professional experiences page by default.
   { href: '/experiences/professional', label: 'Experiences' },
